9dee6f3a8916afabc9e3b8c540f5ba8a79dbd146
Holger Danielsen <holgerdanielsen@MacBook-Air-tilhrende-Holger.local>
2025-04-28 14:37:38 +0200

    sdfds

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const Dropbox = require('dropbox-v2-api');
+const ExcelJS = require('exceljs');
+
+async function main() {
+    console.log('Starter script...');
+    // Din kode her
+}
+
+main();
