0846e67517c5090db587f998e662bd4d759fd092
Holger Danielsen <holgerdanielsen@MacBook-Air-tilhrende-Holger.local>
2025-05-05 16:53:50 +0200

    sfd

--- index.js
+++ index.js
@@ -89,7 +89,11 @@ async function archiveProcessedFile(sourcePath) {
   });
 }
 
-//WUP WUP 
+//
+
+//OPRETTELSE AF FAKTURA
+
+///
 
 /**
  * Downloads any file from Dropbox as Buffer
@@ -138,8 +142,10 @@ async function downloadFile(filePath) {
         bookType: 'xlsx'
       });
   
-      const fileName = products[0].fileName.replace(/\.csv$/, '.xlsx');
-      const destinationPath = `/Teamsport-Invoice/${fileName}`;
+        const iso = new Date().toISOString().replace(/[:.]/g, '-');
+        const baseName = products[0].fileName.replace(/\.csv$/, '');
+        const fileName = `${baseName}_${iso}.xlsx`;
+        const destinationPath = `/Teamsport-Invoice/${fileName}`;
   
       await new Promise((resolve, reject) => {
         const uploadStream = dropbox({
